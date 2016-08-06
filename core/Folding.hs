ef4a9623ada840106e71ad05ec98d17ef3f73bf5
Yuri Gorshenin <y@mapswithme.com>
2016-08-07 00:52:31 +0300

    Implemented solution size threshold.

--- core/Folding.hs
+++ core/Folding.hs
@@ -6,8 +6,6 @@ import Geom
 -- Histroy of mirrors
 type History a = [Line a]
 
-maxSteps = 100
-
 -- A single wrap step - selects a single polygon that can be folded to
 -- cover some part of |figure|, and performs that folding.
 step :: (Fractional a, Ord a, Show a) => Polygon a -> [(Polygon a, History a)] -> [(Polygon a, History a)]
@@ -26,5 +24,8 @@ step figure polygons = case [(l, cs) | l <- lines, let cs = candidates l, not $ 
 -- |maxSteps| steps, returns a list of final facets (possibly
 -- intersecting) with a histry, that can be used to recover a crease
 -- pattern.
-wrap :: (Fractional a, Ord a, Show a) => Polygon a -> Polygon a -> [(Polygon a, History a)]
-wrap figure paper = last . take maxSteps $ iterate (step figure) [(paper, [])]
+wrap :: (Fractional a, Ord a, Show a) => Polygon a -> Polygon a -> [[(Polygon a, History a)]]
+wrap figure paper = go [(paper, [])]
+    where go ps | ps == ps' = [ps]
+                | otherwise = ps : go ps'
+              where ps' = step figure ps
